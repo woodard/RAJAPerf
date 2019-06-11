9836a5d40075ed06ce4f057ebbe05e3658b92887
Ben Woodard <woodard@redhat.com>
2019-06-11 09:34:24 -0700

    Added support for fedora 30 as well. Basically the same as RHEL8 but with a different prefix. The cuda variant needs some work because cuda 10.1 doesn't yet support GCC9

---
+++ scripts/distro-builds/f30.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright (c) 2019 Ben Woodard Red Hat Inc.
+# Copyright (c) 2017-19, Lawrence Livermore National Security, LLC
+# and RAJA Performance Suite project contributors.
+# See the RAJAPerf/COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (BSD-3-Clause)
+#################################################################################
+
+BUILD_SUFFIX=f30
+# The host config for f30 is the same as for RHEL8
+RAJA_HOSTCONFIG=../scripts/distro-builds/rhel8.gcc.cmake
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=On \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
